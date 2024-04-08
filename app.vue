bcafe6150bf482f5e456703229ac58a224e0a651
Jonathan Flower <jfbloom22@gmail.com>
2024-04-08 10:37:18 -0400

    added ogimage and now failing to deploy because of platform error

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+useSeoMeta({
+  title: 'About page',
+  description: 'This is the about page. It uses the same template.',
+});
+defineOgImageComponent('NuxtSeo', {
+  title: 'Welcome to Nuxt OG Image',
+  description: 'I can modify the og:image by changing these props.',
+  icon: 'carbon:help',
+  colorMode: 'dark',
+});
+</script>
 <template>
   <div>
     <NuxtWelcome />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['nuxt-og-image', 'nuxt-icon'],
+  ogImage: {
+    runtimeChromium: false,
+    // defaults: {
+    //   width: 200,
+    //   height: 200,
+    // }
+  },
   nitro: {
     firebase: {
       gen: 2
